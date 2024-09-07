d9111d2c27c43fcaba514a7c2a30d2f4f08722e7
CSBrad <57268178+CSBrad@users.noreply.github.com>
2024-09-07 01:00:18 +0100

    Removed All files and added to new branch called Old_ASAM

--- startApplication.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-tasklist | find /i "Brads Digital Server Manager BDSM.exe" > nul
-if errorlevel 1 (
-    start "" "Brads Digital Server Manager BDSM.exe" /console
-) else (
-    echo Application is already running.
-)
